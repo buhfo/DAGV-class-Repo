84866c541c8dbdcdef5a94e123a0a67f66ce2cb4
buhfo <65921679+buhfo@users.noreply.github.com>
2024-01-24 14:59:10 -0700

    Updates Variables.cs to include a float

--- Variables.cs
+++ Variables.cs
@@ -10,6 +10,7 @@ public class Program
 		bool boolEx = true;
 		int wholeNumber = 10;
 		long longEx = 15000000000;
+		float floatEx = 1.1f;
 		
 		Console.WriteLine(name + " Is an example of a string, " + wholeNumber + " is an integer, " + doubleExample + " is an example of a double, " + longEx + " is an example of long, " + letterEx + " is an example of a char, and " + boolEx + " is an example of a bool.");
 	}

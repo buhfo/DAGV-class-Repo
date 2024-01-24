48f2c20951ccff9cd8967c665c2cc2709cc1b93c
buhfo <65921679+buhfo@users.noreply.github.com>
2024-01-24 14:40:15 -0700

    Created Variables.cs

---
+++ Variables.cs
@@ -0,0 +1,16 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		string name = "Quinn";
+		double doubleExample = 19.99D;
+		char letterEx = 'X';
+		bool boolEx = true;
+		int wholeNumber = 10;
+		long longEx = 15000000000;
+		
+		Console.WriteLine(name + " Is an example of a string, " + wholeNumber + " is an integer, " + doubleExample + " is an example of a double, " + longEx + " is an example of long, " + letterEx + " is an example of a char, and " + boolEx + " is an example of a bool.");
+	}
+}

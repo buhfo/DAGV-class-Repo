87ad46995db1d468b6d3aae66dfe39a605478078
buhfo <65921679+buhfo@users.noreply.github.com>
2024-02-05 14:06:06 -0700

    Created WhileLoops.cs

---
+++ WhileLoops.cs
@@ -0,0 +1,15 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		int i = 0;
+		while( i < 20)
+		{
+			Console.WriteLine(i);
+			i++;
+		}
+		Console.WriteLine("TWENTY!!");
+	}
+}
